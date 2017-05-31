4130e42a7944bb09f4d9567fb53d86a5baed1786
Nicoals Chotard <nchotard@in2p3.fr>
2017-05-31 07:48:48 +0200

    setup for different shells

--- anaconda_setup.sh
+++ anaconda_setup.sh
@@ -1,2 +1,12 @@
-export PATH=$HOME/anaconda3/bin:$PATH
-export PYTHONPATH=$HOME/anaconda3/lib
+# If you have choosen a different path than your home directory for
+# your anaconda install, change the following line
+ANACONDA=$HOME
+
+if command -v export >/dev/null; then
+    export PATH=$ANACONDA/anaconda3/bin:$PATH
+    export PYTHONPATH=$ANACONDA/anaconda3/lib
+else
+    setenv PATH $ANACONDA/anaconda3/bin:$PATH
+    setenv PYTHONPATH $ANACONDA/anaconda3/lib
+fi
+
